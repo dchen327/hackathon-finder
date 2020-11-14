32b5d0ae707b95287f7102dbe4187520e3ba4b7e
David Chen <davidc3287@gmail.com>
2020-11-14 12:59:49 -0500

    parse for eligibility

--- devpost_scraper.py
+++ devpost_scraper.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 
 def get_page(url):
     """ Returns soup object for a provided url """
-    r = requests.get(hackathons_url)
+    r = requests.get(url)
     return BeautifulSoup(r.content, features='lxml')
 
 
-page_num = 2
+page_num = 1
 hackathons_url = f'https://devpost.com/hackathons?page={page_num}'
 soup = get_page(hackathons_url)
 
@@ -16,6 +16,8 @@ soup = get_page(hackathons_url)
 hackathons = soup.find_all('article', {'class': 'challenge-listing'})
 for hackathon in hackathons:
     url = hackathon.a.get('href')
+    print(url)
     soup = get_page(url)
-    print(soup.find_all('article'))
+    eligibility = soup.find(id='challenge-eligibility')
+    print(eligibility.get_text().strip().replace('\n', '  '))
     break
