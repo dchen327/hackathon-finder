bc249979f64e5098ae9b3a0f001ab8964c71e494
David Chen <davidc3287@gmail.com>
2020-11-14 13:19:28 -0500

    format and refactor

--- devpost_scraper.py
+++ devpost_scraper.py
@@ -23,7 +23,6 @@ for page_num in range(1, 11):
     hackathons_url = f'https://devpost.com/hackathons?page={page_num}'
     soup = get_page(hackathons_url)
 
-    # print(soup.prettify())
     hackathons = soup.find_all('article', {'class': 'challenge-listing'})
     for hackathon in hackathons:
         name = hackathon.find('h2').text.strip()
@@ -31,10 +30,6 @@ for page_num in range(1, 11):
             name = '<No Name>'
         print(name)
         url = hackathon.a.get('href')
-        soup = get_page(url)
-        eligibility = soup.find(id='challenge-eligibility')
-        if eligibility is None:
-            continue
-        eligibility_text = eligibility.get_text().strip()
-        print(eligibility_text.lstrip('Eligibility').strip())
+        eligibility_text = get_eligibility(url)
+        print(eligibility_text)
         print('-' * 80)
