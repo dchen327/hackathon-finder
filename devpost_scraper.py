17f20b8926e640cc9b2e07e9353667df6efc70d7
David Chen <davidc3287@gmail.com>
2020-11-14 12:50:52 -0500

    add get request for hackathon pages

--- devpost_scraper.py
+++ devpost_scraper.py
@@ -1,12 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def get_page(url):
+    """ Returns soup object for a provided url """
+    r = requests.get(hackathons_url)
+    return BeautifulSoup(r.content, features='lxml')
+
+
 page_num = 2
 hackathons_url = f'https://devpost.com/hackathons?page={page_num}'
-r = requests.get(hackathons_url)
-soup = BeautifulSoup(r.content, features='lxml')
+soup = get_page(hackathons_url)
 
 # print(soup.prettify())
-hackathons = soup.find_all('h2')
+hackathons = soup.find_all('article', {'class': 'challenge-listing'})
 for hackathon in hackathons:
-    print(hackathon.text.strip())
+    url = hackathon.a.get('href')
+    soup = get_page(url)
+    print(soup.find_all('article'))
+    break
