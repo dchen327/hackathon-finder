9f3f755d1339b4a3fd44f2aef218884898cdd7eb
David Chen <davidc3287@gmail.com>
2020-11-14 13:12:02 -0500

    add multipage support

--- devpost_scraper.py
+++ devpost_scraper.py
@@ -8,22 +8,22 @@ def get_page(url):
     return BeautifulSoup(r.content, features='lxml')
 
 
-page_num = 1
-hackathons_url = f'https://devpost.com/hackathons?page={page_num}'
-soup = get_page(hackathons_url)
+for page_num in range(1, 11):
+    hackathons_url = f'https://devpost.com/hackathons?page={page_num}'
+    soup = get_page(hackathons_url)
 
-# print(soup.prettify())
-hackathons = soup.find_all('article', {'class': 'challenge-listing'})
-for hackathon in hackathons:
-    name = hackathon.find('h2').text.strip()
-    if len(name) <= 1:
-        name = '<No Name>'
-    print(name)
-    url = hackathon.a.get('href')
-    soup = get_page(url)
-    eligibility = soup.find(id='challenge-eligibility')
-    if eligibility is None:
-        continue
-    eligibility_text = eligibility.get_text().strip()
-    print(eligibility_text.lstrip('Eligibility').strip())
-    print('-' * 80)
+    # print(soup.prettify())
+    hackathons = soup.find_all('article', {'class': 'challenge-listing'})
+    for hackathon in hackathons:
+        name = hackathon.find('h2').text.strip()
+        if len(name) <= 1:
+            name = '<No Name>'
+        print(name)
+        url = hackathon.a.get('href')
+        soup = get_page(url)
+        eligibility = soup.find(id='challenge-eligibility')
+        if eligibility is None:
+            continue
+        eligibility_text = eligibility.get_text().strip()
+        print(eligibility_text.lstrip('Eligibility').strip())
+        print('-' * 80)
